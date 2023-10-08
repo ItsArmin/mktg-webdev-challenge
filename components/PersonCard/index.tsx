31d4eb25148f6e69c54fc8d62dac0c4efac79a7d
Armin <17698170+ItsArmin@users.noreply.github.com>
2023-10-07 17:28:52 -0700

    Basic PersonCard

---
+++ components/PersonCard/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import { PersonRecord } from 'types'
+import s from './style.module.css'
+
+interface Props {
+	personRecord: PersonRecord
+}
+
+// TODO: improve styling
+const PersonCard = ({ personRecord }: Props) => {
+	const defaultAvatar = `../../static/images/noAvatar.png`
+	const avatar = personRecord.avatar?.url || defaultAvatar
+
+	return (
+		<div className={`person-record ${s.root}`}>
+			<img src={avatar} alt={personRecord.avatar?.alt ?? 'default avatar'} />
+			<h3>{personRecord.name}</h3>
+			<p>{personRecord.title}</p>
+			<p>{personRecord.department.name}</p>
+		</div>
+	)
+}
+
+export default PersonCard
